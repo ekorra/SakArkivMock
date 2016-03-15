cf5936a119e329ef0b449a3e0af560f90c51d903
Espen Kørra <ekorra@gmail.com>
2016-03-15 10:17:04 +0100

    fjernet FIKS specifikke projekter

--- BestEduServiceStub.Console/MessageSender.cs
+++ BestEduServiceStub.Console/MessageSender.cs
@@ -37,24 +37,9 @@ namespace EduBestServiceStub.Console
 
         public bool SendMessage(string receiverOrgnr)
         {
-            var fiksObject = new Mapper.FiksDataObject
-            {
-                DokumentetsDato = GetDateString(),
-                Journalaar = DateTime.Now.Year.ToString(),
-                Journaldato = GetDateString(),
-                Journalpostnummer = "1",
-                Journalposttype = "U",
-                Journalseksvensnummer = "1234",
-                Journalstatus = "R",
-                ReceiverOrgNr = receiverOrgnr,
-                SenderOrgNr = sernderOrgNr,
-                Saksbehandler = "En saksbehandler",
-                Saksekvensnummer = "1",
-                Saksaar = DateTime.Now.Year.ToString(),
-                Tittel = "En tittel"
-            };
-
-            var request = fiksObject.GetBestEduMelding();
+            
+            //fiks
+            var request = new PutMessageRequestType();
             PutMessageResponseType result;
 
             try
