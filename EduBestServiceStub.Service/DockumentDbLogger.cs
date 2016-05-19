506c3624abd05ed33059a5c49d6a8e3ae68202e5
Espen Kørra <ekorra@gmail.com>
2016-05-19 10:25:45 +0200

    Lagt til docomentDb refferanse

---
+++ EduBestServiceStub.Service/DockumentDbLogger.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using EduBestServiceStub.Lib;
+using EduBestServiceStub.Lib.NoarkTypes;
+
+namespace EduBestServiceStub.Service
+{
+    public class DockumentDbLogger : IRequestLogger
+    {
+        public void Log(PutMessageRequestType request)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
